a7329094690591a7db7a1e4ea69013573f833762
Christoph Döpmann <doepmanc@informatik.hu-berlin.de>
2016-02-07 16:17:51 +0100

    Simplify SignalQueue and add make it FIFO-aware

--- signalqueue.py
+++ signalqueue.py
@@ -6,31 +6,28 @@ import time
 
 class SignalQueue(queue.Queue):
     '''A thread-safe queue that triggers a given event whenever new data is
-    inserted.
-    
-    In order to avoid race conditions, the Event should always be reset BEFORE
-    getting data from the queue.'''
+    inserted.'''
 
-    def __init__(self, event, *args, **kwargs):
+    def __init__(self, events, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.event = event
+        self.events = events
 
     def put(self, *args, **kwargs):
         super().put(*args, **kwargs)
-        self.event.set()
+        self.events.put(self)
 
 
 class QueueSet:
     '''A set of queues that can be waited for simultaneously.'''
     
     def __init__(self):
-        self.event = threading.Event()
+        self.events = queue.Queue()
         self.queues = [ ]
 
     def New(self):
         '''Create a new Queue-like object that can be waited for by this set'''
 
-        new_queue = SignalQueue(self.event)
+        new_queue = SignalQueue(self.events)
         self.queues.append(new_queue)
 
         return new_queue
@@ -46,34 +43,23 @@ class QueueSet:
     def get(self, timeout=None):
         '''Get the next available input as a (queue,data) pair, blocking if
         necessary. If no data was received within the optionally given timeout,
-        an exception of type queue.Empty is raised.
-
-        Note that this operation does not respect the FIFO property between
-        multiple queues, so e.g. starvation may occur.'''
-            
+        an exception of type queue.Empty is raised.'''
+        
         while True:
-            # first check if there is data ready
-            for q in self.queues:
-                try:
-                    data = q.get_nowait()
-                    return (q, data)
-                except queue.Empty:
-                    pass
+            start_wait = time.monotonic()
+            from_queue = self.events.get(timeout=timeout)
 
-            if self.event.wait(timeout):
-                self.event.clear()
+            if from_queue not in self.queues:
+                # wait for next queue entry, because this queue has been
+                # removed in the meantime
+                
+                timeout = time.monotonic() - start_wait
+
+                if timeout <= 0.0:
+                    raise queue.Empty()
+
+                continue
+            
             else:
-                raise queue.Empty()
+                return (from_queue, from_queue.get())
 
-
-#class LockedEvent(threading.Event):
-#    '''An event notifier that can also be locked. It can only be reset, but not
-#    set during a lock phase.
-#
-#    This is useful if used in a consumer-producer scheme for signalling and the
-#    consumer needs to reset the Event based on some conditions. Checking this
-#    condition and resetting the Event can then be done atomically to avoid
-#
-#    # NOT necessary if the consumer always resets the event before looking at
-#    # the queues and just waits for it to be set again when no queue contains
-#    # a value anymore
